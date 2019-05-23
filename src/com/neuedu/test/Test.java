5557370f4944c4db4960d4447dfe4a576705df75
炎 <2682607127@qq.com>
2019-05-23 21:49:19 +0800

    test

--- src/com/neuedu/test/Test.java
+++ src/com/neuedu/test/Test.java
@@ -2,6 +2,14 @@ package com.neuedu.test;
 
 public class Test {
     public static void main(String[] args) {
+        int i = 146325;
+        int m = 0;
+        while (i != 0) {
+            m =i%10;
+            System.out.print(m);
+            i = i/10;
+        }
         System.out.println(1);
     }
+
 }
